03d2a1c7d0275871dc722774f818579e275be6a0
Alex Shelton <tbfunkymonkey@gmail.com>
2016-08-06 14:48:42 -0400

    adjusted code, can have user signed in, via sessions controller and routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params["user"]["email"])
+
+    if @user && @user.authenticate(params["user"]["password"])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "You Have successfully logged in!"
+    else
+      redirect_to new_session_path, notice: "You suck, Try again!"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+  resources :tweets
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
